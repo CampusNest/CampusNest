9be1fcec195f8dc4d8952e002c32fbd8e8ee3ebe
Adeshina Qudus <qudusa55@gmail.com>
2024-05-22 12:58:07 +0100

    new commit

---
+++ src/main/java/com/semicolon/campusnestproject/exception/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package com.semicolon.campusnestproject.exception;
+
+public class UserNotFoundException extends CampusNestException {
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
